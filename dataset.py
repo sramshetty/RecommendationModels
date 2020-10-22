38411c84132daad482b61de057c1b89e1d420f10
Shivaen Ramshetty <44125228+sramshetty@users.noreply.github.com>
2020-10-21 22:51:32 -0400

    full data

--- dataset.py
+++ dataset.py
@@ -175,7 +175,7 @@ class DatasetAttn(object):
 			action_seq_arr_total = data_seq_arr
 
 		if data_name == "taobao":
-			action_seq_arr_total = data_seq_arr[:400]
+			action_seq_arr_total = data_seq_arr
 
 		seq_num = len(action_seq_arr_total)
 		print("seq num", seq_num)
