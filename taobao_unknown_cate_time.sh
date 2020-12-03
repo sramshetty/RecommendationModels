c3c85bf1c8f4f41aad4fd15b7712c97ebe5b9246
Shivaen Ramshetty <44125228+sramshetty@users.noreply.github.com>
2020-12-03 13:51:03 -0500

    RNN tests

--- taobao_unknown_cate_time.sh
+++ taobao_unknown_cate_time.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 CUDA_LAUNCH_BLOCKING=1 python main_time.py --data_folder ../Data/tmall/100k_unknown_cate/ --data_name taobao --embedding_dim 300 --hidden_size 300 --lr 0.005 --model_name 'GRU4REC'
+CUDA_VISIBLE_DEVICES=0 CUDA_LAUNCH_BLOCKING=1 python main_time.py --data_folder ../Data/tmall/100k_unknown_cate/ --data_name taobao --embedding_dim 300 --hidden_size 300 --lr 0.005 --num_layers 2 --model_name 'GRU4REC'
