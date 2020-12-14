cbaa85f4edae8d8f4e527f26b56d4258bfa5bae2
Shivaen Ramshetty <44125228+sramshetty@users.noreply.github.com>
2020-12-14 18:04:24 -0500

    add items to eval

--- metric_shiv.py
+++ metric_shiv.py
@@ -1,10 +1,47 @@
 import torch
 
+# def get_recall(indices, targets, mask):
+#     targets = targets.view(-1, 1).expand_as(indices)
+#     hits = (targets == indices)
+        
+#     hits *= mask.view(-1, 1).expand_as(indices)
+#     hits = hits.nonzero()
+
+#     recall = float(hits.size(0)) / float( mask.int().sum())
+
+#     return recall
+
+
+# def get_mrr(indices, targets, mask):
+#     tmp = targets.view(-1, 1)
+#     targets = tmp.expand_as(indices)
+#     hits = (targets == indices)
+#     hits *= mask.view(-1, 1).expand_as(indices)
+    
+#     hits = hits.nonzero()
+#     ranks = hits[:, -1] + 1
+#     ranks = ranks.float()
+#     rranks = torch.reciprocal(ranks)
+#     mrr = torch.sum(rranks).data / float( mask.int().sum())
+#     return mrr.item()
+
+
+# def evaluate(indices, targets, mask, k=20, debug=False):
+#     _, indices = torch.topk(indices, k, -1)
+    
+#     indices = indices.cpu()
+#     targets = targets.cpu()
+
+#     recall = get_recall(indices, targets, mask)
+#     mrr = get_mrr(indices, targets, mask)
+
+#     return recall, mrr
+
 def get_recall(indices, targets, mask):
-    targets = targets.view(-1, 1).expand_as(indices)
+    targets = targets.view(-1).expand_as(indices)
     hits = (targets == indices)
         
-    hits *= mask.view(-1, 1).expand_as(indices)
+    hits *= mask.view(-1).expand_as(indices)
     hits = hits.nonzero()
 
     recall = float(hits.size(0)) / float( mask.int().sum())
@@ -13,13 +50,13 @@ def get_recall(indices, targets, mask):
 
 
 def get_mrr(indices, targets, mask):
-    tmp = targets.view(-1, 1)
+    tmp = targets.view(-1)
     targets = tmp.expand_as(indices)
     hits = (targets == indices)
-    hits *= mask.view(-1, 1).expand_as(indices)
+    hits *= mask.view(-1).expand_as(indices)
     
     hits = hits.nonzero()
-    ranks = hits[:, -1] + 1
+    ranks = hits[-1] + 1
     ranks = ranks.float()
     rranks = torch.reciprocal(ranks)
     mrr = torch.sum(rranks).data / float( mask.int().sum())
