e0819ce805daf5bc755a7ef66e1d3d925cc856f6
Shivaen Ramshetty <44125228+sramshetty@users.noreply.github.com>
2020-12-14 01:37:32 -0500

    add items to eval

--- evaluation_shiv.py
+++ evaluation_shiv.py
@@ -123,6 +123,7 @@ class Evaluation(object):
             for input_x_batch, target_y_batch, idx_batch in dataloader:
                 input_x_batch = input_x_batch.to(self.device)
                 target_y_batch = target_y_batch.to(self.device)
+                print(input_x_batch, target_y_batch)
                 warm_mask = (idx_batch >= self.warm_start)
 
                 logit_batch = self.model(input_x_batch)
