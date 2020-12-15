853e2400b8116008083e4e4fd422778bf090e0e1
Shivaen Ramshetty <44125228+sramshetty@users.noreply.github.com>
2020-12-15 14:46:22 -0500

    add items to eval

--- metric_shiv.py
+++ metric_shiv.py
@@ -29,15 +29,15 @@ def get_recall(indices, targets, mask):
 
 
 def get_mrr(indices, targets, mask):
-    tmp = targets.view(-1, 1)
-    targets = tmp.expand_as(indices)
+    targets = targets.view(-1, 1).expand_as(indices)
     hits = (targets == indices)
     hits *= mask.view(-1, 1).expand_as(indices)
-    
     hits = hits.nonzero()
+
     ranks = hits[:, -1] + 1
     ranks = ranks.float()
     rranks = torch.reciprocal(ranks)
+    print(rranks, rranks.size())
     mrr = torch.sum(rranks).data / float( mask.int().sum())
     return mrr.item()
 
